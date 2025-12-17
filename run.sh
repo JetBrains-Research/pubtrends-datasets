4b6e1b439e7b118cdda3d5d372ba46f61fa1d959
Momir Milutinovic <milutinm462@gmail.com>
2025-12-17 12:13:59 +0100

    Switch to the uv package manager

--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# conda init
-conda_install_path=$(conda info | grep -i 'base environment' | awk '{print $4}')
-source $conda_install_path/etc/profile.d/conda.sh
-conda env update -f environment.yml
-conda activate pubtrends_datasets
-
-python -m flask --app src.app.app run --port 5002
+uv run -- flask --app src.app.app run --port 5002
