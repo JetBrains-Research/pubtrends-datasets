ea47082b9663f06bfa655d78c27fa55883a906cf
Momir Milutinovic <milutinm462@gmail.com>
2026-01-13 17:59:16 +0100

    Initial test for geometadb backfilling tool

--- src/test/db/test_backfill_geometadb.py
+++ src/test/db/test_backfill_geometadb.py
@@ -2,9 +2,12 @@ import datetime
 import unittest
 from unittest.mock import Mock, AsyncMock, patch
 import gzip
+import asyncio
 
 from src.config.config import Config
-from src.db.backfill_geometadb import GEOmetadbBackfiller
+from src.db.backfill_geometadb import GEOmetadbBackfiller, RETRY_ATTEMPTS
+from src.test.helpers.async_iterator import AsyncIterator
+
 
 class TestGEOmetadbBackfiller(unittest.TestCase):
     def setUp(self):
@@ -12,36 +15,53 @@ class TestGEOmetadbBackfiller(unittest.TestCase):
         self.test_config = Config(test=True)
         self.repository = Mock()
         self.repository.save_gses_async = AsyncMock()
+        self.repository.get_gses_async = AsyncMock(return_value=[])
         self.backfiller = GEOmetadbBackfiller(self.test_config, self.repository)
 
     @staticmethod
-    def _make_response(gse_accession: str):
+    def _make_response(gse_accession: str, valid_body=True):
         raw_body = "\n".join([f"^SERIES = {gse_accession}", "!Series_title = Title",
                                   f"!Series_geo_accession = {gse_accession}", f"!Series_pubmed_id = 12345"])
-        body = gzip.compress(raw_body.encode("utf-8"))
+        body = gzip.compress(raw_body.encode("utf-8")) if valid_body else b"INVALID"
         mock_response = AsyncMock()
         mock_response.read.return_value = body
         mock_response.status = 200
+        mock_response.content = Mock()
+        mock_response.content.iter_chunked.side_effect = lambda chunk_size: AsyncIterator([body])
         return mock_response
 
     @patch("src.db.backfill_geometadb.get_geo_ids")
     @patch("aiohttp.ClientSession.get")
     def test_backfill_geometadb_success(self, mock_get: Mock, mock_get_accessions: Mock):
-        mock_get.return_value.__aenter__.return_value = self._make_response("GSE12345")
-        mock_get_accessions.return_value = ["GSE12345"]
+        mock_get.return_value.__aenter__.return_value = self._make_response("GSE000000")
+        mock_get_accessions.return_value = ["GSE000000"]
         start_date, end_date = datetime.datetime(2025, 1, 1), datetime.datetime(2025, 1, 2)
         datasets = self.backfiller.backfill_geometadb(start_date, end_date)
         mock_get_accessions.assert_called_once()
         mock_get.assert_called_once()
         self.assertEqual(len(datasets), 1)
-        self.assertEqual(datasets[0].gse, "GSE12345")
+        self.assertEqual(datasets[0].gse, "GSE000000")
         self.repository.save_gses_async.assert_called_once()
 
-    def test_backfill_geometadb_download_failure(self):
-        pass
+    @patch("src.db.backfill_geometadb.get_geo_ids")
+    @patch("aiohttp.ClientSession.get")
+    def test_backfill_geometadb_download_failure(self, mock_get: Mock, mock_get_accessions: Mock):
+        mock_get.side_effect = asyncio.TimeoutError("Download failed")
+        mock_get_accessions.return_value = ["GSE000000"]
+        start_date, end_date = datetime.datetime(2025, 1, 1), datetime.datetime(2025, 1, 2)
+        self.assertRaises(asyncio.TimeoutError, self.backfiller.backfill_geometadb, start_date, end_date)
+        mock_get_accessions.assert_called_once()
+        self.assertEqual(mock_get.call_count, RETRY_ATTEMPTS)
 
-    def test_backfill_geometadb_invalid_gzip(self):
-        pass
+    @patch("src.db.backfill_geometadb.get_geo_ids")
+    @patch("aiohttp.ClientSession.get")
+    def test_backfill_geometadb_invalid_gzip(self, mock_get: Mock, mock_get_accessions: Mock):
+        mock_get.return_value.__aenter__.return_value = self._make_response("GSE000000", valid_body=False)
+        mock_get_accessions.return_value = ["GSE000000"]
+        start_date, end_date = datetime.datetime(2025, 1, 1), datetime.datetime(2025, 1, 2)
+        self.assertRaises(gzip.BadGzipFile, self.backfiller.backfill_geometadb, start_date, end_date)
+        mock_get_accessions.assert_called_once()
+        self.assertEqual(mock_get.call_count, RETRY_ATTEMPTS)
 
     def test_backfill_geometadb_parse_failure(self):
         pass
