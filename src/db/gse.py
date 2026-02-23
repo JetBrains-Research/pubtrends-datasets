5af9d9dc7dda5dfd9813080f3dad830ded385c87
MomirMilutinovic <40830508+MomirMilutinovic@users.noreply.github.com>
2026-02-23 13:01:16 +0100

    GEOmetadb backfilling tool (#13)
    * Data pipeline for backfilling geometadb
    
    * Make error handling in GEOmetadbBackfiller more robust
    
    * Check database integrity when constructing GSERepository
    
    * Make end_date optional when running the geometadb backfilling script
    
    * Geometadb backfilling script
    
    * Make removing files asnyc in backfill_geometadb
    
    * Write files in chunks instead of all at once
    
    * Improve error handling in geometadb backfiller and add progress bar
    
    * Log GSE accessions that failed to be backfilled
    
    * Initial test for geometadb backfilling tool
    
    * More unit tests for backfilling tool
    
    * Fix test docker container configuration
    
    * Switch from raw sqlite to SQLAlchemy
    
    * Save accessions of datasets that need to be updated in each backfilling run
    
    * Add a status field to update jobs and individual dataset updates
    
    * Add database migrations
    
    * Upsert in GSERepository instead of insert
    
    * Expose info about backfilling jobs via API endpoints
    
    * Remove redundant GEOmetadbGSELoader
    
    * Make GEOmetadbUpdateJobRepository.set_gse_update_status_async an AsyncMock instead of Mock in test_backfill_geometadb
    
    * Add python version file
    
    * Remove unused commits in app.py
    
    * Clean up geometadb backfilling implementation
    
    * Formatting and fixing qodana issues
    
    * Log every time a datasets is donwloaded in NCBIGSELoader
    
    * Rename tables and classes related to geometadb update jobs for increased clarity
    
    * Add geometadb backfilling tool section to README
    
    * Change ram exhaustion block in README to warning
    
    * Add that March 6th 2024 is the geometadb cutoff date
    
    * Add how to run database migrations to README.md
    
    * Fix docker tests
    
    * Make config.properties a mounted file in docker tests
    
    * Add instructions on how to run the backfilling tool using cron
    
    * Add invalid date range test for geometadb backfilling tool
    
    * Note in README that migrations need to be run after setup
    
    * Run database migrations in setup.sh
    
    * Use os.path.expanduser when parsing paths in config.properties
    
    * Keep altertantive geometadb download link in setup.sh
    
    * Extract ESearch base url in get_geo_accessions_for_dates.py
    
    * Remove database for logging GEOmetadb update jobs

--- src/db/gse.py
+++ src/db/gse.py
@@ -1,28 +1,39 @@
 """Gene Expression Omnibus Series (GSE) data model."""
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 
+from src.db.mapper_registry import mapper_registry
 
+from sqlalchemy import Index, Integer, PrimaryKeyConstraint, REAL, Text, Column
+
+
+@mapper_registry.mapped
 @dataclass
 class GSE:
     """Gene Expression Omnibus Series (GSE) data model."""
+    __tablename__ = 'gse'
+    __table_args__ = (
+        PrimaryKeyConstraint('gse', name='pk_gse'),
+        Index('gse_acc_idx', 'gse')
+    )
+    __sa_dataclass_metadata_key__ = "sa"
 
-    ID: Optional[float] = None
-    title: Optional[str] = None
-    gse: Optional[str] = None
-    status: Optional[str] = None
-    submission_date: Optional[str] = None
-    last_update_date: Optional[str] = None
-    pubmed_id: Optional[int] = None
-    summary: Optional[str] = None
-    type: Optional[str] = None
-    contributor: Optional[str] = None
-    web_link: Optional[str] = None
-    overall_design: Optional[str] = None
-    repeats: Optional[str] = None
-    repeats_sample_list: Optional[str] = None
-    variable: Optional[str] = None
-    variable_description: Optional[str] = None
-    contact: Optional[str] = None
-    supplementary_file: Optional[str] = None
+    ID: Optional[float] = field(default=None, metadata={"sa": Column(REAL)})
+    title: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    gse: Optional[str] = field(default=None, metadata={"sa": Column(Text, primary_key=True)})
+    status: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    submission_date: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    last_update_date: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    pubmed_id: Optional[int] = field(default=None, metadata={"sa": Column(Integer)})
+    summary: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    type: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    contributor: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    web_link: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    overall_design: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    repeats: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    repeats_sample_list: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    variable: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    variable_description: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    contact: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
+    supplementary_file: Optional[str] = field(default=None, metadata={"sa": Column(Text)})
---
+++ src/helpers/remove_if_exists.py
@@ -0,0 +1,28 @@
+import errno
+from pathlib import Path
+from typing import Union
+
+import aiofiles
+import aiofiles.os
+
+
+async def async_file_exists(path: Union[Path, str]) -> bool:
+    """
+    Checks asynchronously if a file or path exists.
+    """
+    try:
+        await aiofiles.os.stat(str(path))
+        return True
+    except FileNotFoundError:
+        return False
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            return False
+        raise
+    except ValueError:
+        return False
+
+
+async def async_remove_if_exists(path):
+    if await async_file_exists(path):
+        await aiofiles.os.remove(path)
