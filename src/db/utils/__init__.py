d807c60a000eb17708ed391e261b6866491ee55d
Momir Milutinovic <milutinm462@gmail.com>
2026-03-26 12:41:05 +0100

    fix: Fix warning 'src.db.utils.backfill_geometadb' found in sys.modulesFix failing semantic search tests

--- src/db/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.db.utils.backfill_geometadb import GEOmetadbBackfiller
-from src.db.utils.get_geo_accessions_for_dates import get_gse_ids_by_last_update_date
-
-__all__ = ["GEOmetadbBackfiller", "get_gse_ids_by_last_update_date"]
