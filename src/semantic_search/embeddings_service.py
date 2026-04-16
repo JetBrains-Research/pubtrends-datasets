a6ee739934c721fb3d04da7730c9964abd635b6a
Momir Milutinovic <milutinm462@gmail.com>
2026-04-16 12:27:01 +0200

    perf: Use requests.session in embeddings_service.py to speed up http requests

--- src/semantic_search/embeddings_service.py
+++ src/semantic_search/embeddings_service.py
@@ -14,12 +14,11 @@ class EmbeddingsServiceError(Exception):
 
 @tenacity.retry(wait=tenacity.wait_exponential(max=10), stop=tenacity.stop_after_attempt(3),
                 before_sleep=tenacity.before_sleep_log(logger, logging.WARNING), reraise=True)
-def fetch_texts_embedding_batch(texts, embeddings_service_url):
+def fetch_texts_embedding_batch(texts, embeddings_service_url, session: requests.Session):
     logger.debug('Fetch texts embeddings')
     try:
-        r = requests.request(
+        r = session.post(
             url=f'{embeddings_service_url}/embeddings_texts',
-            method='POST',
             json=texts,
             headers={'Accept': 'application/json'},
             timeout=10,
@@ -56,8 +55,9 @@ def fetch_texts_embedding(texts, embeddings_service_url, batch_size=256):
     for i in range(0, len(texts), batch_size):
         texts_batches.append(texts[i:i + batch_size])
     embeddings = []
-    for texts_batch in texts_batches:
-        embeddings.extend(fetch_texts_embedding_batch(texts_batch, embeddings_service_url))
+    with requests.Session() as session:
+        for texts_batch in texts_batches:
+            embeddings.extend(fetch_texts_embedding_batch(texts_batch, embeddings_service_url, session))
     embeddings = np.vstack(embeddings)
     if len(embeddings) != len(texts):
         raise ValueError(f'Expected {len(texts)} embeddings, got {embeddings.shape[0]}')
